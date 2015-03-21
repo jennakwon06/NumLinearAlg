43c6f54f49a3ff23ff451078a7163847c07aa3b8
Jenna Kwon <jinkwon06@gmail.com>
2015-03-21 10:38:55 -0400

    readme for TheHilbertMatrix completed

--- TheHilbertMatrix/lu_fact.py
+++ TheHilbertMatrix/lu_fact.py
@@ -1,9 +1,48 @@
+# Author: Jenna Kwon
 import numpy as np
+import sys
+import pprint
+
+# input : .dat file
+# Output :  L, U, and error |LU-A|inf
 
 
-# input : A, b
+def lu_fact(file_name):
 
-# return L, U, and error |LU-A|inf
+    if file_name is None:
+        exit()
+
+    # A = ndarray
+    A = np.loadtxt(file_name, unpack=True)
+
+    # Dimension
+    n = A.shape[0]
+
+    # Initialize L & U.
+    # L = I, U = A for Doolittle algorithm
+    L = np.identity(n, dtype='d')
+    U = A
+
+    # LU fact without partial pivoting
+    # Doolittle algorithm is used
+    # Assumes that factorization exists
+    # Aij = sum of Lip*Upj, p = counter
+    # Fill in L below the diagonal
+    # Lij = (aij - sum of lip * upj) / ujj. i > j
+    # Uij = (aij - sum of lip  * upj).
+    # Fill in U above the diagonal
+    for i in range(0, n - 1):
+        for j in range(i + 1, n):
+            L[j, i] = U[j, i] / U[i, i]
+            U[j, i:] = U[j, i:] - L[j, i] * U[i, i:]
+            U[j, i] = 0
+
+    print A, L, U
 
 
-#display Q and R on the command line
+# For when lu_fact is used as a stand-alone module
+# Read command line argument. Must be exactly one argument.
+if __name__ == '__main__':
+    lu_fact(sys.argv[1])
+# else:
+#     #do nothing
